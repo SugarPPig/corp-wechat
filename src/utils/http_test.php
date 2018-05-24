e36dfc49775d134166495a605c77bf7a5fac22a1
liaotian-mac <417296832@qq.com>
2018-05-24 16:50:02 +0800

    改造中

---
+++ src/utils/http_test.php
@@ -0,0 +1,8 @@
+<?php
+
+use sugarfishes\corpWechat\HttpUtils;
+// $output = HttpUtils::httpGet("http://localhost:8080");
+// var_dump($output);
+
+$output = HttpUtils::httpPost("http://localhost:8080", "hello");
+// var_dump($output);
---
+++ src/utils/test.php
@@ -0,0 +1,21 @@
+<?php
+use sugarfishes\corpWechat\utils\Utils;
+
+
+class A {
+    public $a = "aaaa";
+    public $b = 111;
+}
+
+class B {
+    public $c = "cccc";
+    public $d = null; // A array
+}
+
+$a = new A();
+
+$b = new B();
+
+$b->b = array($a, $a);
+
+// var_dump(Utils::Object2Array($b));

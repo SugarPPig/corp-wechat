19c52dffd9914b42df3da1e3ff79f95672632d2c
liaotian-mac <417296832@qq.com>
2018-05-25 09:42:15 +0800

    config

--- src/config.php
+++ src/config.php
@@ -1,13 +1,5 @@
-<?php /*
- * Copyright (C) 2017 All rights reserved.
- *   
- * @File config.php
- * @Brief 
- * @Author abelzhu, abelzhu@tencent.com
- * @Version 1.0
- * @Date 2017-12-27
- *
- */
+<?php
+
 
 return array(
     'DEBUG' => false, // 是否打印debug信息
